43c05881075bf40f3b6c7c62c6758c86f411c2cc
maykim0525 <maykim1823@gmail.com>
2024-11-26 16:56:16 +0900

    11-26

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,49 +1,77 @@
 import React, { useState } from "react";
-
 import { headerNav } from "../constants";
 
+// MUI 아이콘 및 컴포넌트 임포트
+import {
+	HomeRounded,
+	AssignmentIndRounded,
+	LanRounded,
+	DevicesRounded,
+	PermContactCalendarRounded,
+} from "@mui/icons-material";
+import { Tooltip, Switch } from "@mui/material";
+
 const Header = () => {
-    const [show, setShow] = useState(false);
+	const [darkMode, setDarkMode] = useState(false);
 
-    const toggleMenu = () => {
-        setShow((prevShow) => !prevShow);
-    }
+	const handleDarkModeToggle = () => {
+		setDarkMode(!darkMode);
+		// 여기에 다크 모드 스타일 적용 로직 추가 가능
+	};
 
-    return (
-        <header id="header" role="banner">
-            <div className="header__inner">
-                <div className="header__logo">
-                    <h1>
-                        <a href="/">portfolio<em>react.js</em></a>
-                    </h1>
-                </div>
-                <nav 
-                    className={`header__nav ${show ? "show" : ""}`} 
-                    role="navigation" 
-                    aria-label="메인메뉴"
-                >
-                    <ul>
-                        {headerNav.map((nav, key) => (
-                            <li key={key}>
-                                <a href={nav.url}>{nav.title}</a>
-                            </li>
-                        ))}
-                    </ul>
-                </nav>
-                <div 
-                    className="header__nav__mobile" 
-                    id="headerToggle" 
-                    aria-controls="primary-menu" 
-                    aria-expanded={show ? "true" : "false"} 
-                    role="button"
-                    tabIndex="0"
-                    onClick={toggleMenu}
-                >
-                    <span></span>
-                </div>
-            </div>
-        </header>
-    )
-}
+	const icons = {
+		main: <HomeRounded />,
+		about: <AssignmentIndRounded />,
+		skills: <LanRounded />,
+		project: <DevicesRounded />,
+		contact: <PermContactCalendarRounded />,
+	};
+
+	return (
+		<header id="header" role="banner">
+			<div className="header__nav-container">
+				<nav
+					className="header__nav"
+					role="navigation"
+					aria-label="메인메뉴"
+				>
+					<ul>
+						{headerNav.map((nav, key) => (
+							<li key={key}>
+								<Tooltip
+									title={
+										nav.title
+									}
+									arrow
+								>
+									<a
+										href={
+											nav.url
+										}
+									>
+										{icons[
+											nav.title.toLowerCase()
+										] ||
+											null}
+									</a>
+								</Tooltip>
+							</li>
+						))}
+					</ul>
+					{/* 다크모드 토글 */}
+					<div className="header__darkmode-toggle">
+						<Switch
+							checked={darkMode}
+							onChange={
+								handleDarkModeToggle
+							}
+							color="primary"
+						/>
+					</div>
+				</nav>
+			</div>
+		</header>
+	);
+};
 
 export default Header;
